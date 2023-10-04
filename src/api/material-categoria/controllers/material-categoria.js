5dff68207800dba30e01a88ae79a82ec825ebfac
joao <rollbuschplay@hotmail.com>
2023-10-04 19:13:20 -0300

    feat: organizando as tabelas e criando campos novos

---
+++ src/api/material-categoria/controllers/material-categoria.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * material-categoria controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::material-categoria.material-categoria');
